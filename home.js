ad3b14a37acd39913ef3e7f4318c626980aad112
Yuki2t1 <163606067+Yuki2t1@users.noreply.github.com>
2025-02-21 08:02:58 -0800

    Update home.js

--- home.js
+++ home.js
@@ -1,34 +1,42 @@
 let container=document.querySelector("#container")
 let button=document.createElement("button")
 let button1=document.createElement("button")
-
 button.textContent="CHOOSE GRID SIZE"
 button.setAttribute("id","btn1")
 document.body.appendChild(button)
-
 button1.setAttribute("id","btn2")
 document.body.appendChild(button1)
 button1.textContent="Reset"
 
 
-button.addEventListener("click",()=>{
-    let x=prompt("choose the x size of the grid:"+" ")
-    let y=prompt("choose the y size of the grid:"+" ")
-    if(x >100 || y>100){
-        alert("choose a value lower than 100"+" ")
-        x=null
-        y=null
+function grid(){
+    let x=prompt("Give ME A SIZE FOR THE GRID MR.S OR MS.SIGMA (need to be lower then 100).")
+    if(x>100){
+        x=0
+        alert("GIVE A NUMBER NUMBER THAN 100.")
+    }else if(x===""||x===" "||x<1){
+        x=0
+        alert("GIVE ME A NUMBER BIGGER THAN ONE OR NOTHING.")
+    }else if(isNaN(x)){
+        x=0 
+        alert("WORD ARE NOT ACCEPTABLE.")
     }
+    container.innerHTML=""
     
-    for(i=0;i<x*y;i++){
-        const div=document.createElement("div")
-        div.classList.add("div1") 
-        container.appendChild(div)
+    for(i=0;i<x*x;i++){    
+        let row = document.createElement("div");
+        row.classList.add("div1"); 
+        container.appendChild(row);  
+        
+        }
         
 }
-})
+
+
+button.addEventListener("click",grid)
+
 button1.addEventListener("click",()=>{
-    container.remove()
+    container.innerHTML=""
         
 })
 
